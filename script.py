b3fa22ba891288a803dd907b21ad21fd96b94ed7
marissabc <mcarper@terpmail.umd.edu>
2025-04-16 15:45:55 -0400

    testing

--- script.py
+++ script.py
@@ -109,7 +109,8 @@ if __name__ == "__main__":
         {"course_id": "ANTH222", "section_id": "0103", "instructor": "", "term_id": "202508"},
         {"course_id": "ANTH222", "section_id": "0104", "instructor": "", "term_id": "202508"},
         {"course_id": "ANTH222", "section_id": "0105", "instructor": "", "term_id": "202508"},
-        {"course_id": "ANTH222", "section_id": "0106", "instructor": "", "term_id": "202508"}
+        {"course_id": "ANTH222", "section_id": "0106", "instructor": "", "term_id": "202508"},
+        {"course_id": "BSST200", "section_id": "0101", "instructor": "", "term_id": "202508"}
 
     ]
 
